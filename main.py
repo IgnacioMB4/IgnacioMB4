50f8a8ce1d3d8d466966255a74030567f07252f7
IgnacioMB4 <99254749+IgnacioMB4@users.noreply.github.com>
2024-02-27 14:06:42 +0100

    ddasd

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 from googleapiclient.http import MediaFileUpload
 from Google import Create_Service
 
-CLIENT_SECRET_FILE = "trchatbot.json"
-API_NAME = "drive"
-API_VERSION = "v3"
-SCOPES = ["https://www.googleapis.com/auth/drive"]
+def uploadToDrive(filename, folder):
+    CLIENT_SECRET_FILE = "trchatbot.json"
+    API_NAME = "drive"
+    API_VERSION = "v3"
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-folder_id = "10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
-file_names = ["imagen2.jpg"]
-mime_types = ["image/jpeg   <-- en el caso de que se una imagen"]
+    folder_id = folder #"10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
+    file_names = [filename]
+    mime_types = ["image/jpeg   <-- en el caso de que se una imagen"]
 
-for file_name, mime_type in zip(file_names, mime_types):
-    file_metadata = {
-        "name" : file_name,
-        "parents" : [folder_id]
-    }
+    for file_name, mime_type in zip(file_names, mime_types):
+        file_metadata = {
+            "name" : file_name,
+            "parents" : [folder_id]
+        }
 
-    media = MediaFileUpload(file_name, mimetype=mime_type)
+        media = MediaFileUpload(file_name, mimetype=mime_type)
 
-    service.files().create(
-        body=file_metadata,
-        media_body = media,
-        fields = "id"
-    ).execute()
-print("\n------------------------")
-print("          BIEN")
-print("------------------------")
+        service.files().create(
+            body=file_metadata,
+            media_body = media,
+            fields = "id"
+        ).execute()
--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,6 @@
+from Google import Create_Service
+from main import uploadToDrive
 print("HOLA")
+filename = "10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
+file = "imagen2.jpg"
+uploadToDrive(file,filename)

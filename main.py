e65d547eea7599e86942704e83b767a9ed12195c
IgnacioMB4 <99254749+IgnacioMB4@users.noreply.github.com>
2024-02-27 13:40:11 +0100

    sip

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from googleapiclient.http import MediaFileUpload
+from Google import Create_Service
 
+CLIENT_SECRET_FILE = "trchatbot.json"
+API_NAME = "drive"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
 
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+folder_id = "10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
+file_names = ["imagen2.jpg"]
+mime_types = ["image/jpeg   <-- en el caso de que se una imagen"]
+
+for file_name, mime_type in zip(file_names, mime_types):
+    file_metadata = {
+        "name" : file_name,
+        "parents" : [folder_id]
+    }
+
+    media = MediaFileUpload(file_name, mimetype=mime_type)
+
+    service.files().create(
+        body=file_metadata,
+        media_body = media,
+        fields = "id"
+    ).execute()
 print("\n------------------------")
 print("          BIEN")
 print("------------------------")
-for i in range (0,5):
-    print(i)

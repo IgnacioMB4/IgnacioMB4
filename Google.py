c03aa0af8ffacae8bb7ae206809944b503fb18d4
IgnacioMB4 <99254749+IgnacioMB4@users.noreply.github.com>
2024-02-27 14:16:49 +0100

    sip

--- Google.py
+++ Google.py
@@ -46,3 +46,29 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
 def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
     dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
     return dt
+
+def uploadToDrive(filename, folder):
+    CLIENT_SECRET_FILE = "trchatbot.json"
+    API_NAME = "drive"
+    API_VERSION = "v3"
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    folder_id = folder #"10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
+    file_names = [filename]
+    mime_types = ["image/jpeg   <-- en el caso de que se una imagen"]
+
+    for file_name, mime_type in zip(file_names, mime_types):
+        file_metadata = {
+            "name" : file_name,
+            "parents" : [folder_id]
+        }
+
+        media = MediaFileUpload(file_name, mimetype=mime_type)
+
+        service.files().create(
+            body=file_metadata,
+            media_body = media,
+            fields = "id"
+        ).execute()
--- nuevo.py
+++ nuevo.py
@@ -1,6 +1,5 @@
-from Google import Create_Service
-from main import uploadToDrive
-print("HOLA")
+from Google import Create_Service, uploadToDrive
+
 filename = "10Br7QI-GgkWx3ZlOHdtZumh7UzdGAfcq"
 file = "imagen2.jpg"
 uploadToDrive(file,filename)

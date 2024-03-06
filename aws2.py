eb18d4d73774c2385cc0cdde5ba9571bebce6e30
IgnacioMB4 <99254749+IgnacioMB4@users.noreply.github.com>
2024-03-06 18:56:22 +0100

    bhj

--- aws2.py
+++ aws2.py
@@ -5,4 +5,4 @@ AWS_SECRET_ACCESS_KEY = "YOQbQIP1+91K7gAGBj8QB8hrWv8uI0vA+3ieBzrg"
 
 client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-client.upload_file('imagen2.jpg', 'clivi-infinite', 'prueba.jpg')
+client.upload_file('imagen2.jpg', 'clivi-infinite', 'nano.jpg')
